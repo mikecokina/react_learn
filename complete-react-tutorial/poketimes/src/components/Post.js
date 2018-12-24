0f5ddcbf913afd763607a896db9b2ec8479f5ba4
s-t-e-a-l-t-h <mikecokina@gmail.com>
2018-12-24 17:58:46 +0100

    own props (params) to redux

--- complete-react-tutorial/poketimes/src/components/Post.js
+++ complete-react-tutorial/poketimes/src/components/Post.js
@@ -1,33 +1,17 @@
 import React, { Component } from 'react'
-import axios from "axios";
+import { connect } from 'react-redux'
 
 
 class Post extends Component {
-    state = {
-        post: null
-    };
-
-    componentDidMount() {
-        let id = this.props.match.params.post_id;
-
-        axios.get('https://jsonplaceholder.typicode.com/posts/' + String(id)
-        ).then(
-        res => {
-            this.setState({
-                post: res.data
-            })
-        })
-    }
-
     render() {
 
-        const post = this.state.post ? (
+        const post = this.props.post ? (
             <div className="post">
                 <h4 className="center">
-                    {this.state.post.title}
+                    {this.props.post.title}
                 </h4>
                 <p>
-                    {this.state.post.body}
+                    {this.props.post.body}
                 </p>
             </div>
         ) : (
@@ -42,4 +26,11 @@ class Post extends Component {
     }
 }
 
-export default Post
+const mapStateToProps = (state, ownProps) => {
+    let id = parseInt(ownProps.match.params.post_id);
+    return {
+        post: state.posts.find(post => post.id === id)
+    }
+};
+
+export default connect(mapStateToProps)(Post);
